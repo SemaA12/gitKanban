d061717e20e91cfab346a99072fb2bef4b380dd2
EnesA99 <enes.alici@compax.at>
2024-04-20 13:14:08 +0200

    implement TaskRepository

--- KanbanBoard/src/main/java/com/example/KanbanBoard/repository/TaskRepository.java
+++ KanbanBoard/src/main/java/com/example/KanbanBoard/repository/TaskRepository.java
@@ -1,14 +1,50 @@
 package com.example.KanbanBoard.repository;
-
+import java.util.ArrayList;
+import java.util.List;
 import com.example.KanbanBoard.data.Task;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Repository
 @AllArgsConstructor
 public class TaskRepository {
-    private final List<Task> tasks;
 
-}
+
+        private List<Task> tasks;
+
+        public TaskRepository() {
+            this.tasks = new ArrayList<>();
+        }
+
+        public void addTask(Task task) {
+            tasks.add(task);
+        }
+
+        public void updateTask(Task updatedTask) {
+            for (int i = 0; i < tasks.size(); i++) {
+                if (tasks.get(i).getTaskID() == updatedTask.getTaskID()) {
+                    tasks.set(i, updatedTask);
+                    return;
+                }
+            }
+            System.out.println("Task with ID " + updatedTask.getTaskID() + " not found.");
+        }
+
+        public void deleteTask(int taskId) {
+            for (int i = 0; i < tasks.size(); i++) {
+                if (tasks.get(i).getTaskID() == taskId) {
+                    tasks.remove(i);
+                    return;
+                }
+            }
+            System.out.println("Task with ID " + taskId + " not found.");
+        }
+
+        public List<Task> getTasks() {
+            return tasks;
+        }
+    }
+
+
